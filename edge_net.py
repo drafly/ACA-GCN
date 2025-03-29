8842f1baea7201c62f75e1c047a47cbb54f04dab
NingA-20000 <168540020+NingA-20000@users.noreply.github.com>
2025-03-29 15:45:20 +0800

    Update edge_net.py

--- edge_net.py
+++ edge_net.py
@@ -30,7 +30,7 @@ class weight(torch.nn.Module):
 
 
         if flag == 1:
-            # edge replacement
+            # EWP
             selected = edge_weight[edge_weight < 0.85]  # 0.85
             n = len(selected)
             idx = torch.nonzero(edge_weight < 0.85).squeeze()
@@ -41,13 +41,13 @@ class weight(torch.nn.Module):
 
 
         if flag == 2:
-            # 边丢失 保留了权重大于等于0.85的边
+            # Adaptive EP
             mask = edge_weight >= 0.85
             edge_index = edge_index[:, mask]
             edge_weight = edge_weight[mask]
 
         if flag == 3:
-            # 随机的边置换
+            # Random EWP
             n = len(edge_weight)  # 0.85
             n1 = int(0.3 * n)
             al_idx = np.arange(n)
@@ -56,18 +56,4 @@ class weight(torch.nn.Module):
                 j = random.randint(i, n1 - 1)
                 idx_i, idx_j = idx[i], idx[j]
                 edge_weight[idx_i], edge_weight[idx_j] = edge_weight[idx_j], edge_weight[idx_i]
-
-            # 边扰动
-            # def add_noise_to_edge_weights(edge_weight, p, noise_scale):
-            #     num_edges = edge_weight.size(0)
-            #     num_noise = int(p * num_edges)
-            #     noise_indices = random.sample(range(num_edges), num_noise)
-            #     new_edge_weight = edge_weight.clone()
-            #     for index in noise_indices:
-            #         noise = torch.randn_like(new_edge_weight[index]) * noise_scale
-            #         new_edge_weight[index] += noise
-            #     return new_edge_weight
-            # p = 0.3  # 添加噪声到30%的边
-            # noise_scale = 0.1  # 噪声的尺度
-            # edge_weight = add_noise_to_edge_weights(edge_weight, p, noise_scale)
         return edge_weight, edge_index
